0f4da5c6144c5365f07a2c425cf2d8910a0f5135
Leigh Ericksen <109568123+stokedesco@users.noreply.github.com>
2025-06-09 22:26:16 +1000

    Handle non-array options

--- includes/class-sh-schema.php
+++ includes/class-sh-schema.php
@@ -6,24 +6,34 @@ class SH_Schema {
     public function output_schema(){
         list($weekly, $holidays) = SH_Shortcodes::get_data();
         $schema = array();
-        foreach($weekly as $day=>$v){
-            if (!empty($v['closed'])) continue;
-            $schema[] = array(
-                '@type'=>'OpeningHoursSpecification',
-                'dayOfWeek'=> $day,
-                'opens'=> $v['open'],
-                'closes'=> $v['close'],
-            );
+
+        if (is_array($weekly)) {
+            foreach ($weekly as $day => $v) {
+                if (!empty($v['closed'])) {
+                    continue;
+                }
+                $schema[] = array(
+                    '@type'     => 'OpeningHoursSpecification',
+                    'dayOfWeek' => $day,
+                    'opens'     => $v['open'],
+                    'closes'    => $v['close'],
+                );
+            }
         }
-        foreach($holidays as $h){
-            if (isset($h['closed'])) continue;
-            $schema[] = array(
-                '@type'=>'OpeningHoursSpecification',
-                'validFrom'=>$h['from'],
-                'validThrough'=>$h['to'],
-                'opens'=>$h['open'],
-                'closes'=>$h['close'],
-            );
+
+        if (is_array($holidays)) {
+            foreach ($holidays as $h) {
+                if (isset($h['closed'])) {
+                    continue;
+                }
+                $schema[] = array(
+                    '@type'        => 'OpeningHoursSpecification',
+                    'validFrom'    => $h['from'],
+                    'validThrough' => $h['to'],
+                    'opens'        => $h['open'],
+                    'closes'       => $h['close'],
+                );
+            }
         }
         echo "<script type='application/ld+json'>".json_encode($schema)."</script>";
     }
